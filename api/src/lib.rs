1aa1e471d902e8cd1f4efce7d867f3a93e8394eb
PetoMPP <piotreq22@gmail.com>
2023-07-23 02:29:42 +0200

    chore: reorganize code for testing

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,4 +1,8 @@
 use std::env;
+use crate::controllers::users::UsersController;
+use crate::extensions::extension::Extension;
+use crate::controllers::controller::ControllerRegisterer;
+use rocket::{Config, Rocket, Build};
 
 pub mod auth;
 pub mod controllers;
@@ -14,4 +18,16 @@ impl Default for Secrets {
         let api_secret = env::var("API_SECRET").unwrap_or("shhhdonttellanyoneaboutit".to_string());
         Self { api_secret }
     }
+}
+
+pub fn build_rocket() -> Rocket<Build> {
+    Extension(rocket::build())
+        .add(UsersController)
+        .into()
+        .manage(Secrets::default())
+        .configure(Config {
+            port: 16969,
+            address: "0.0.0.0".parse().unwrap(),
+            ..Default::default()
+        })
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,22 +1,9 @@
-use petompp_web_api::{
-    controllers::{controller::ControllerRegisterer, users::UsersController},
-    extensions::extension::Extension,
-    Secrets,
-};
-use rocket::Config;
+use petompp_web_api::build_rocket;
 
 #[macro_use]
 extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
-    Extension(rocket::build())
-        .add(UsersController)
-        .into()
-        .manage(Secrets::default())
-        .configure(Config {
-            port: 16969,
-            address: "0.0.0.0".parse().unwrap(),
-            ..Default::default()
-        })
+    build_rocket()
 }
